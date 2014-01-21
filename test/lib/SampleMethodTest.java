938085f992307cc4336ac8730150c26fd138dec4
mohit <mohitsha@thoughtworks.com>
2014-01-21 17:19:26 +0530

    It should work now.

--- test/lib/SampleMethodTest.java
+++ test/lib/SampleMethodTest.java
@@ -3,9 +3,6 @@ package lib;
 import junit.framework.Assert;
 import org.junit.Test;
 
-/**
- * Created by mohitsha on 1/20/14.
- */
 public class SampleMethodTest {
     @Test
     public void testIsItWorking() throws Exception {
